ff84ddb80c5e09dedbf05817970a113e623407a7
Neth <nethsrey34@gmail.com>
2017-06-07 15:41:56 -0700

    mind

---
+++ web/dbconfig.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neth
+ * Date: 07-Jun-17
+ * Time: 3:31 PM
+ */
+$connect = mysqli_connect('localhost','root','');
+mysqli_select_db($connect,'pcphoto');
---
+++ web/insert.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Neth
+ * Date: 07-Jun-17
+ * Time: 3:35 PM
+ */
