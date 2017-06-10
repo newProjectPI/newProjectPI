5b9ff8e9653d6fa028ffd5080b61af0d1e18401b
DESKTOP-8M9EVK5\ACER <sayseth999@gmail.com>
2017-06-10 12:23:59 +0700

    new

--- web/index.php
+++ web/index.php
@@ -15,6 +15,7 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/style_s.css">
 <!-- //Custom Theme files -->
 <!-- font-awesome icons -->
 <link href="css/font-awesome.css" rel="stylesheet"> 
@@ -38,7 +39,7 @@
 </script>
 <!-- //end-smooth-scrolling --> 
 </head> 
-<body>
+<body style="background: #f2f2f2">
 	<!-- for bootstrap working -->
 	<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
 	<!-- //for bootstrap working -->
@@ -46,11 +47,9 @@
 	
 	<!-- header modal -->
 	<!-- header -->
-	<div class="header" id="home1">
+	<div class="header" id="home1" >
 		<div class="container">
-			<div class="w3l_login">
-				<a href="#" data-toggle="modal" data-target="#myModal88"><img src="images/logo.png" width="80" height="80"></a>
-			</div>
+
 			<div class="w3l_logo">
 				<h1 align="center"><a href="index.php">Panda PC Shop<span>Your stores. Your place.</span></a></h1>
 			</div>
@@ -75,11 +74,11 @@
 	</div>
 	<!-- //header -->
 	<!-- navigation -->
-	<div class="navigation">
+	<div class="navigation" style="background: #737373">
 		<div class="container">
 			<nav class="navbar navbar-default">
 				<!-- Brand and toggle get grouped for better mobile display -->
-				<div class="navbar-header nav_2">
+				<div class="navbar-header nav_2" >
 					<button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
 						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar"></span>
@@ -133,58 +132,14 @@
 		</div>
 	</div>
 	<!-- //navigation -->
-	<!-- slide show -->
-	<div >
-		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel" style="width: 100%;" data-interval="3000">
-			<!-- Indicators -->
-			<ol class="carousel-indicators">
-				<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-				<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-			</ol>
 
-			<!-- Wrapper for slides -->
-			<div class="carousel-inner" role="listbox">
-				<div class="item">
-					<img src="laptop/acer/dell.jpg" alt=""  style="margin-left: auto; margin-right:auto; width: 100%;height: 100%">
-					<div class="carousel-caption">
-						<!-- 	title -->
-					</div>
-				</div>
-				<div class="item">
-					<img src="laptop/acer/microsoft.jpg" alt=""  style="margin-left: auto; margin-right:auto; width: 100%;height: 100%" >
-					<div class="carousel-caption">
-						<!-- 	title -->
-					</div>
-				</div>
-				<div class="item active">
-					<img src="laptop/acer/acer-aspire.jpg" alt=""  style="margin-left: auto; margin-right:auto;width: 100%;height: 100%">
-					<div class="carousel-caption">
-						<!-- 	title -->
-					</div>
-				</div>
-			</div>
-			<!-- Controls -->
-			<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		</div>
-	</div>
-	<div id="showtext" style="width:100%; height: 40px; font-size: 25px; ">
-		<marquee>សូមស្វាគមន៏មកកាន់គេហទំព័ររបស់យើងខ្ញុំ លោកអ្នកអាចធ្វើការទំនាក់ទំនងដើម្បីផ្សពផ្សាយពានិជ្ជកម្មក្នុងតម្លៃសម្រម។ </marquee>
-	</div>
 	<!-- //banner --> 
 	<!-- banner-bottom -->
 	<?php
 	$msg = "";
 	if (isset($_POST['upload'])) {
 		$target = "images/".basename($_FILES['image']['name']);
-		$db = mysqli_connect("localhost", "root", "", "pcphoto");
+		$db = mysqli_connect("localhost", "root", "", "pcphotc");
 		$photo = $_FILES['image']['name'];
 		$title = $_POST['title'];
 		$description = $_POST['text'];
@@ -200,199 +155,25 @@
 	?>
 	
 	<?php
-	$db = mysqli_connect("localhost","root","","pcphoto");
+	$db = mysqli_connect("localhost","root","","pcphotc");
 	$sql = "SELECT * FROM computer";
 	$result = mysqli_query($db,$sql);
-	while ($row = mysqli_fetch_array($result)) {
-		echo "<div id='img_div'>";
-		echo "<img src='images/".$row['photo']."'>";
-		echo "<p>".$row['title']."</p>";
-		echo "<p>".$row['description']."</p>";
-		echo "</div>";
-	}
+	echo "<div class=' col-md-12' style='margin-top: 10px'>";
+	echo "<div style='background: white;' class='modal-content col-md-offset-1 col-md-10' >";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<div id='img_div'>";
+            echo "<img src='images/".$row['photo']."'>";
+            echo "<p>".$row['title']."</p>";
+            echo "<p>".$row['description']."</p>";
+            echo "</div>";
+        }
+	echo "</div>";
+    echo "</div>";
 	?>
-	
 
-
-	<!-- //banner-bottom --> 
-	<!-- modal-video -->
-	<div class="modal video-modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-						<div class="col-md-5 modal_body_left">
-							<img src="Asus/Asus4.jpg" alt=" " class="img-responsive" />
-						</div>
-						<div class="col-md-7 modal_body_right">
-							<h4>The Best Computer</h4>
-							<p>ASUS ROG G752VS (2017)
-								- Intel i7-6820HK (2.70GHz) <br>
-								- RAM: 16GB DDR4 (Speed 2133 MHz) <br>
-								- HDD : 1TB + 512GB PCIe <br>
-								- Optical Drive: DVD-RW <br>
-								- VGA 1 : Intel HD Graphics 530 <br>
-								- VGA 2 : NVIDIA GTX 1070 (8GB) <br>
-								- Gigabit Ethernet, Bluetooth 4.1 <br>
-								- Battery: 8 Cell 88 Wh 6000 mAh <br>
-								- Weight : 3.5 Kg <br>
-								- Windows 10 License </p>
-							<div class="rating">
-								<div class="rating-left">
-									<img src="images/star-.png" alt=" " class="img-responsive" />
-								</div>
-								<div class="rating-left">
-									<img src="images/star-.png" alt=" " class="img-responsive" />
-								</div>
-								<div class="rating-left">
-									<img src="images/star-.png" alt=" " class="img-responsive" />
-								</div>
-								<div class="rating-left">
-									<img src="images/star.png" alt=" " class="img-responsive" />
-								</div>
-								<div class="rating-left">
-									<img src="images/star.png" alt=" " class="img-responsive" />
-								</div>
-								<div class="clearfix"> </div>
-							</div>
-							<div class="modal_body_right_cart simpleCart_shelfItem">
-								<p><span>$780</span> <i class="item_price">$750</i></p>
-								<form action="#" method="post">
-									<input type="hidden" name="cmd" value="_cart">
-									<input type="hidden" name="add" value="1"> 
-									<input type="hidden" name="w3ls_item" value="Laptop1"> 
-									<input type="hidden" name="amount" value="750.00">   
-									<button type="submit" class="w3ls-cart">Add to cart</button>
-								</form>
-							</div>
-
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModal1">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-						<div class="col-md-5 modal_body_left">
-							<img src="images/9.jpg" alt=" " class="img-responsive" />
-						</div>
-
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModal2">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal3" tabindex="-1" role="dialog" aria-labelledby="myModal3">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-
-						</div>
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal4" tabindex="-1" role="dialog" aria-labelledby="myModal4">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal5" tabindex="-1" role="dialog" aria-labelledby="myModal5">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<div class="modal video-modal fade" id="myModal6" tabindex="-1" role="dialog" aria-labelledby="myModal6">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
-				</div>
-				<section>
-					<div class="modal-body">
-
-						<div class="clearfix"> </div>
-					</div>
-				</section>
-			</div>
-		</div>
-	</div>
-	<!-- //modal-video -->
-	<!-- banner-bottom1 -->
-	<div class="banner-bottom1">
-		<div class="agileinfo_banner_bottom1_grids">
-			<div class="col-md-7 agileinfo_banner_bottom1_grid_left">
-				<!-- <h3>Grand Opening Event With flat<span>20% <i>Discount</i></span></h3> -->
-			<!-- 	<a href="products.html">Shop Now</a> -->
-			</div>
-			<div class="col-md-5 agileinfo_banner_bottom1_grid_right">
-				<h4>hot deal</h4>
-				<div class="timer_wrap">
-					<div id="counter"> </div>
-				</div>
-				<!-- <script src="js/jquery.countdown.js"></script> -->
-				<script src="js/script.js"></script>
-			</div> 
-			<div class="clearfix"> </div>
-		</div>
-	</div>
-	<!-- //banner-bottom1 --> 
-	<!-- special-deals -->
 	<div class="special-deals">
 		<div class="container">
-			<h2><b>Special Deals</b></h2>
+			<h2>Special promotion</h2>
 			<div class="w3agile_special_deals_grids">
 				<div class="col-md-7 w3agile_special_deals_grid_left">
 					<div class="w3agile_special_deals_grid_left_grid">
@@ -404,138 +185,95 @@
 							<h4>We Offer <span>Best Products</span></h4>
 						</div>
 					</div>
-					<div>
-						<img src="images/Alienware-14-17-18-family-shot.jpg">
-					</div>
-				</div>
-				
-					<div class="col-md-5 w3agile_special_deals_grid_right">
-					<img src="images/newyear.png" alt=" " class="img-responsive" />
 
-				</div>
+
 				<div class="clearfix"> </div>
 			</div>
 		</div>
 	</div>
 
 	<!-- top-brands -->
+        <div id="footer" class="col-md-12" style="height: 200px;">
+            <hr>
+
+                <div class="container">
+
+                    <div class="sliderfig">
+                        <ul id="flexiselDemo1">
+                            <li>
+                                <img src="images/tb1.jpg" alt=" " class="img-responsive" />
+                            </li>
+                            <li>
+                                <img src="images/dell-logo.jpg" alt=" " class="img-responsive" />
+                            </li> -->
+                            <li>
+                                <img src="images/tb3.jpg" alt=" " class="img-responsive" />
+                            </li>
+                            <!-- <li>
+                                <img src="images/tb4.jpg" alt=" " class="img-responsive" />
+                            </li> -->
+
+                        </ul>
+                    </div>
+                    <script type="text/javascript">
+                        $(window).load(function() {
+                            $("#flexiselDemo1").flexisel({
+                                visibleItems: 4,
+                                animationSpeed: 1000,
+                                autoPlay: true,
+                                autoPlaySpeed: 3000,
+                                pauseOnHover: true,
+                                enableResponsiveBreakpoints: true,
+                                responsiveBreakpoints: {
+                                    portrait: {
+                                        changePoint:480,
+                                        visibleItems: 1
+                                    },
+                                    landscape: {
+                                        changePoint:640,
+                                        visibleItems:2
+                                    },
+                                    tablet: {
+                                        changePoint:768,
+                                        visibleItems: 3
+                                    }
+                                }
+                            });
+
+                        });
+                    </script>
+                    <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+                </div>
+
+            <hr>
+            <div class="col-md-12" style="height: 200px">
+                <div style="height:100%;" class="col-md-4">
+
+                    <a href="#"><h4 align="center"><b>Meet ua</b></h4></a><br>
+                    <a href="#"><h4 align="center">facebook</h4></a><br>
+                    <a href="#"><h4 align="center">Istagram</h4></a><br>
+                    <a href="#"><h4 align="center">twister</h4></a>
+
+
+                </div>
+                <div style="height: 100%;" class="col-md-4">
+                    <a href="#"><h4 align="center">About panda shop</h4></a><br>
+                    <a href="#"><h4 align="center">about us</h4></a>
+
+                </div>
+                <div style="height: 100%;background: blue" class="col-md-4">
+
+                </div>
+
+            </div>
+        </div>
 	
-	<div class="top-brands">
-		<div class="container">
-		
-			<h3><b>Top Brands</b></h3>
-		
-			<div class="sliderfig">
-				<ul id="flexiselDemo1">			
-					<li>
-						<img src="images/tb1.jpg" alt=" " class="img-responsive" />
-					</li> 
-					<li>
-						<img src="images/dell-logo.jpg" alt=" " class="img-responsive" />
-					</li> -->
-					<li>
-						<img src="images/tb3.jpg" alt=" " class="img-responsive" />
-					</li>
-					<!-- <li>
-						<img src="images/tb4.jpg" alt=" " class="img-responsive" />
-					</li> -->
-					<li>
-						<img src="images/lenovo.jpg" alt=" " class="img-responsive" />
-					</li>
-				</ul>
-			</div>
-			<script type="text/javascript">
-					$(window).load(function() {
-						$("#flexiselDemo1").flexisel({
-							visibleItems: 4,
-							animationSpeed: 1000,
-							autoPlay: true,
-							autoPlaySpeed: 3000,    		
-							pauseOnHover: true,
-							enableResponsiveBreakpoints: true,
-							responsiveBreakpoints: { 
-								portrait: { 
-									changePoint:480,
-									visibleItems: 1
-								}, 
-								landscape: { 
-									changePoint:640,
-									visibleItems:2
-								},
-								tablet: { 
-									changePoint:768,
-									visibleItems: 3
-								}
-							}
-						});
-						
-					});
-			</script>
-			<script type="text/javascript" src="js/jquery.flexisel.js"></script>
-		</div>
-	</div>
 
-	<!-- //top-brands --> 
-	<!-- newsletter -->
-	<div class="newsletter">
-		<div class="container">
-			<div class="col-md-6 w3agile_newsletter_left" style="background-color: lightpink">
-				<h4>Comment</h4>
-					<textarea style="width: 200px">
 
-					</textarea>
-				<h3>Newsletter</h3>
-				<p style="color: black;">Sharing your happiness to people around you.</p>
-			</div>
-<!--			<div class="col-md-6 w3agile_newsletter_right">-->
-<!--				<form action="#" method="post">-->
-<!--					<input type="email" name="Email" placeholder="Email" required="">-->
-<!--					<input type="submit" value="" />-->
-<!--				</form>-->
-<!--			</div>-->
-			<div class="col-md-6 w3agile_newsletter_left">
-				<form action="postImage.php">
-					<button type="submit" class="btn btn-info" style="width: 80px; float: right; " >
-						Post
-					</button>
-				</form>
-			</div>
-			<div class="clearfix"> </div>
-		</div>
 
-	</div>
+
 	
-				<div class="clearfix"> </div>
-			</div>
-		</div>
-		<div class="footer-copy">
-			<div class="footer-copy1">
-				<div class="footer-copy-pos">
-					<a href="#home1" class="scroll"><img src="images/arrow.png" alt=" " class="img-responsive" /></a>
-				</div>
-			</div>
-			<div class="container">
-				<p>&copy; 2017 Panda Shop. All rights reserved | Design by <a href="http://Panda_Shop/">Panda Developer</a></p>
-			</div>
-		</div>
-	</div>
-	<!-- //footer -->
-	<!-- cart-js -->
-	<!-- <script src="js/minicart.js"></script>
-	<script>
-        w3ls.render();
 
-        w3ls.cart.on('w3sb_checkout', function (evt) {
-        	var items, len, i;
 
-        	if (this.subtotal() > 0) {
-        		items = this.items();
-
-        		for (i = 0, len = items.length; i < len; i++) {
-        		}
-        	}
-        });
-    </script>   -->
-	<!-- //cart-js -->
 </body>
 </html>
