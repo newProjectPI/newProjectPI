0f91968370d1e7843c09dac69fcf4d7ef8a9e189
DESKTOP-8M9EVK5\ACER <sayseth999@gmail.com>
2017-06-13 13:03:36 +0700

    new

--- web/index.php
+++ web/index.php
@@ -72,13 +72,7 @@
                 </div>
 
             </div><!-- /.col-lg-6 -->
-			<!-- <div class="cart cart box_1"> 
-				<form action="#" method="post" class="last"> 
-					<input type="hidden" name="cmd" value="_cart" />
-					<input type="hidden" name="display" value="1" />
-					<button class="w3view-cart" type="submit" name="submit" value=""><i class="fa fa-cart-arrow-down" aria-hidden="true"></i></button>
-				</form>   
-			</div>   -->
+		
 		</div>
 	</div>
 	<!-- //header -->
