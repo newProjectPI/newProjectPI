5f7b2163fa4613c34333baa4396e5548c071d846
Neth <nethsrey34@gmail.com>
2017-06-16 15:06:51 -0700

    new admin

---
+++ web/admin.php
@@ -0,0 +1,298 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Neth
+ * Date: 16-Jun-17
+ * Time: 2:03 PM
+ */
+<html>
+<head>
+    <title>Page sign up</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="Electronic Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+	SmartPhone Compatible web template, free web designs for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+        function hideURLbar(){ window.scrollTo(0,1); } </script>
+    <!-- //for-mobile-apps -->
+    <!-- Custom Theme files -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/fasthover.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" type="text/css" href="css/style_s.css">
+    <!-- //Custom Theme files -->
+    <!-- font-awesome icons -->
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <!-- //font-awesome icons -->
+    <!-- js -->
+    <script src="js/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/jquery.countdown.css" /> <!-- countdown -->
+    <!-- //js -->
+    <!-- web fonts -->
+    <link href='//fonts.googleapis.com/css?family=Glegoo:400,700' rel='stylesheet' type='text/css'>
+    <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="header navbar-fixed-top" style="background: white;">
+    <div class="container" style="margin-top: -2%">
+        <div style="width: 30%;float: left;">
+            <img src="images/logo.png" style="width: 60px; height: 60px;">
+            <p style="margin-top: 14px"><h2><b>Panda PC Shop</b></h2></p>
+        </div>
+
+        <form role="form" action="search.php" method="get" style="width: 65%;float: left;margin:0;">
+            <div class="row" style=" width: 100%">
+                <div class="col-lg-6" style="width: 80%;">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for..." name="query" >
+                        <span class="input-group-btn">
+                        <button class="btn btn-primary glyphicon glyphicon-search" name="submit" type="submit" style="margin-top: -2%">
+
+                        </button>
+                    </span>
+                    </div><!-- /input-group -->
+
+                </div><!-- /.col-lg-6 -->
+
+            </div><!-- /.row -->
+        </form>
+        <div style="float: right">
+            <button class="btn btn-info fa fa-user" data-toggle="modal" data-target="#myModal" style="margin-top: 15px"></button>
+        </div>
+
+    </div>
+</div>
+
+<!--loing-->
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" >
+    <div class="modal-dialog" role="document" style="width: 60%" >
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel" align="center">Log in</h4>
+            </div>
+            <div class="modal-body">
+                <form class="form-horizontal">
+                    <div class="form-group">
+                        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="inputEmail3" placeholder="Email" style="margin-left: 10%">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="inputPassword3" placeholder="Password" style="margin-left: 10%">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10" style="margin-left: 25%">
+                            <input type="checkbox" name="check">Remember me
+</div>
+                    </div>
+                    <a style="margin-left: 25%">
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+            <hr>
+            <a href="signUp.php" style="float: right;margin-right: 20px;margin-top: -10px">sign up</a>
+            </form>
+
+
+
+
+        </div>
+    </div>
+</div>
+</div>
+<!--navigation-->
+<div class="navigation" style="background: #737373;margin-top: 50px;margin-left: 0">
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header nav_2" >
+                <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse" data-target="#bs-megadropdown-tabs">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
+                <ul class="nav navbar-nav">
+                    <li><a href="index.php" class="act">Home</a></li>
+                    <!-- Mega Menu -->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products <b class="caret"></b></a>
+                        <ul class="dropdown-menu multi-column columns-3">
+                            <div class="row">
+                                <div class="col-md-6">
+
+                                    <ul class="multi-column-dropdown">
+                                        <h6>Laptop</h6>
+                                        <li><a href="products.php">Asus</a></li>
+                                        <li><a href="products.php">Dell <span>New</span></a></li>
+                                        <li><a href="products.php">Lenovo</a></li>
+                                        <li><a href="products.php">MSI<span>New</span></a></li>
+                                    </ul>
+                                </div>
+                                <div class="col-md-6">
+                                    <ul class="multi-column-dropdown">
+
+                                        <h6>Laptop</h6>
+                                        <li><a href="products1.php">Samsung</a></li>
+                                        <li><a href="products1.php">Hp</a></li>
+                                        <li><a href="products1.php">Apple <span>New</span></a></li>
+                                        <!-- <li><a href="products1.html"><i>Summer Store</i></a></li> -->
+                                    </ul>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                        </ul>
+                    </li>
+                    <li><a href="about.php">About Us</a></li>
+
+
+                    <!-- <li class="w3pages"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                             <li><a href="icons.html">Web Icons</a></li>
+                            <li><a href="codes.html">Short Codes</a></li>
+                        </ul>
+                    </li>  -->
+                    <li><a href="mail.php">Contact Us</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
+
+<div id="div_contain" class="col-lg-12" style="height: 300px;margin-top: 10px">
+    <div style="width: 90%;height: 100%;margin: auto">
+        <div style="width: 20%;height: 100%;float: left" class="modal-content">
+            <div class="list-group">
+                <a href="#" class="list-group-item">User</a>
+                <a href="#" class="list-group-item">Chat</a>
+                <a href="#" class="list-group-item">Update Product</a>
+                <a href="#" class="list-group-item">Delete Product</a>
+                <a href="#" class="list-group-item">Insert Product</a>
+                <a href="#" class="list-group-item">List Product</a>
+            </div>
+        </div>
+        <div style="width: 78%;height: 100%;float: right" class="modal-content">
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<div id="footer" class="col-md-12" style="height: 200px;">
+    <hr>
+
+    <div class="container">
+
+        <div class="sliderfig">
+            <ul id="flexiselDemo1">
+                <li>
+                    <img src="images/tb1.jpg" alt=" " class="img-responsive" />
+                </li>
+                <li>
+                    <img src="images/dell-logo.jpg" alt=" " class="img-responsive" />
+                </li> -->
+                <li>
+                    <img src="images/tb3.jpg" alt=" " class="img-responsive" />
+                </li>
+                <!-- <li>
+                    <img src="images/tb4.jpg" alt=" " class="img-responsive" />
+                </li> -->
+
+            </ul>
+        </div>
+        <script type="text/javascript">
+    $(window).load(function() {
+        $("#flexiselDemo1").flexisel({
+                    visibleItems: 4,
+                    animationSpeed: 1000,
+                    autoPlay: true,
+                    autoPlaySpeed: 3000,
+                    pauseOnHover: true,
+                    enableResponsiveBreakpoints: true,
+                    responsiveBreakpoints: {
+            portrait: {
+                changePoint:480,
+                            visibleItems: 1
+                        },
+            landscape: {
+                changePoint:640,
+                            visibleItems:2
+                        },
+            tablet: {
+                changePoint:768,
+                            visibleItems: 3
+                        }
+        }
+                });
+            });
+        </script>
+        <script type="text/javascript" src="js/jquery.flexisel.js"></script>
+    </div>
+
+    <hr>
+    <div class="col-md-12" style="height: 200px">
+        <div style="height:100%;" class="col-md-4">
+            <a href="#">
+                <strong style="color: #000000;">
+    ABOUT PANDA SHOP
+</strong>
+            </a> <br>
+            <a href="#" style="color: darkslategrey">About us</a> <br>
+            <a href="#" style="color: darkslategrey">Careers</a> <br>
+            <a href="#" style="color: darkslategrey">Terms&Conditions</a> <br>
+            <a href="#" style="color: darkslategrey">Privacy Policy</a>
+
+        </div>
+        <div style="height: 100%;" class="col-md-4">
+            <a href="#">
+                <strong style="color: #000000">
+    MEET US ON
+</strong>
+            </a> <br>
+            <a href="#" style="color: darkslategrey">Facebook</a><br>
+            <a href="#" style="color: darkslategrey">Instagram</a><br>
+            <a href="#" style="color: darkslategrey">Twitter</a><br>
+            <a href="#" style="color: darkslategrey">YouTube</a>
+
+        </div>
+        <div class="col-md-4">
+            <a href="#">
+                <strong style="color: #000000">
+    BRANCH
+                </strong>
+            </a> <br>
+            <a style="color: darkslategrey">
+    Kompong Cham
+</a><br>
+            <a style="color: darkslategrey">
+    Battambang
+            </a><br>
+            <a style="color: darkslategrey">
+    Kompong Chnang
+</a><br>
+            <a style="color: darkslategrey">
+    Kompong Speu
+</a>
+        </div>
+
+    </div>
+
+    <div style="margin-bottom: 70px">
+        <p align="center">&copy; 2017 Panda Shop. All rights reserved | Design by <a href="http://panda_shop.com/">Panda Shop</a></p>
+    </div>
+</div>
+</body>
+</html>
