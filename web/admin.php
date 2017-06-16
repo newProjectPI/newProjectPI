5793f9ebc9f25612ed2fcc72c65840f32f522f7c
Neth <nethsrey34@gmail.com>
2017-06-16 15:09:36 -0700

    new admin

--- web/admin.php
+++ web/admin.php
@@ -34,7 +34,7 @@
     <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
-<div class="header navbar-fixed-top" style="background: white;">
+<div class="header navbar-fixed-top" style="background: white">
     <div class="container" style="margin-top: -2%">
         <div style="width: 30%;float: left;">
             <img src="images/logo.png" style="width: 60px; height: 60px;">
